229d79b6aeadda1294496cac3064dcf78a883a03
Jan Macku <jamacku@redhat.com>
2021-07-07 10:22:58 +0200

    remove lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
+use std::env;
 use std::fs::{self, DirEntry};
 use std::path::Path;
 
 use clap::App;
 
-use mac_address::MacAddress;
-
-use rename_device::{
-    read_env_interface,
-    get_mac_address
+use mac_address::{
+    mac_address_by_name,
+    MacAddress
 };
+
+
+// --- --- --- //
 
 const ENV: &str = "INTERFACE";
 const CONFIG_DIR: &str = "/etc/sysconfig/network-scripts";
+
+
+// --- --- --- //
 
 fn main() {
     let kernel_if_name: String;
@@ -61,4 +66,36 @@ fn main() {
     }
 
     // ? print out correct name of interface
+}
+
+
+// --- Functions --- //
+
+/* Read env variable INTERFACE in order to get names of if */
+fn read_env_interface(env_name: &str) -> Option<String> {
+    match env::var_os(env_name) {
+        Some(val) => {
+            match val.into_string() {
+                Ok(val) => Some(val),
+                _ => {
+                    eprintln!("Error whille procesing env INTERFACE: {}.", env_name);
+                    None
+                }
+            }
+        },
+        None => {
+            eprintln!("{} is not defined in the environment.", env_name);
+            None
+        }
+    }
+}
+
+/* Get MAC addres of given interface */
+fn get_mac_address(if_name: &str) -> Option<MacAddress> {
+    match mac_address_by_name(if_name) {
+        Ok(val) => val,
+        _ => {
+            None
+        }
+    }
 }
