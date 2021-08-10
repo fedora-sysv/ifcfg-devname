44b267c72afa2179b32d43b223c733f964482e00
Jan Macku <jamacku@redhat.com>
2021-08-10 14:50:26 +0200

    working version

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::error;
 use std::path::Path;
 use std::fs::File;
-use std::io::ErrorKind;
 use std::io:: {
     prelude::*,
     BufReader
@@ -31,8 +30,7 @@ type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 const ENV: &str = "INTERFACE";
 const CONFIG_DIR: &str = "/etc/sysconfig/network-scripts";
-// const KERNEL_CMD: &str = "/proc/cmdline";
-const KERNEL_CMD: &str = "./fake_kernel_cmd";
+const KERNEL_CMD: &str = "/proc/cmdline";
 
 
 // --- --- --- //
@@ -47,7 +45,7 @@ fn main() {
 
     App::new("rename_device")
         .author("Macku Jan <jamacku@redhat.com>")
-        .about("Program rename_device reads ENV INTERFACE, which is expected to contain the name of the network interface. Then it looks for the hardware address of such an interface. Finally it scans ifcfg configuration files in directory /etc/sysconfig/network-scripts/ and looks for configuration with HWADDR set to given hw address. If the program successfully finds such a configuration, it returns on standard output content of property DEVICE from matching ifcfg configuration. In all other cases it returns error code 1.")
+        .about("Program rename_device reads ENV INTERFACE, which is expected to contain the name of the network interface. Then it looks for the hardware address of such an interface. After that it looks at the kernel command line for key-value-pair ifname=NEW_NAME:MAC_ADDRESS with given mac address. If a new name wasn't found and kernel cmd it scans ifcfg configuration files in directory /etc/sysconfig/network-scripts/ and looks for configuration with HWADDR set to given hw address. If the program successfully finds such a configuration, it returns on standard output content of property DEVICE from matching ifcfg configuration. In all other cases it returns error code 1.")
         .get_matches();
 
     /* Read env variable INTERFACE in order to get names of if */
@@ -220,6 +218,7 @@ fn scan_config_file(config_file: &Path, mac_address: &MacAddress) -> Result<Opti
 }
 
 /* Scan kernel cmd and look for given hardware address and return new device name */
+#[allow(unused)]
 fn scan_kernel_cmd(mac_address: &MacAddress) -> Result<Option<String>> {
     let file: File;
     let reader: BufReader<File>;
