d5a215afbb6166c2371414dcd7bdc071d0cc7d24
Jan Macku <jamacku@redhat.com>
2021-08-17 10:21:22 +0200

    remove clap to save space

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use std::io:: {
     BufReader
 };
 
-use clap::App;
-
 use mac_address:: {
     mac_address_by_name,
     MacAddress
@@ -42,11 +40,6 @@ fn main() {
     let list_of_ifcfg_paths: Vec<String>;
 
     let mut device_config_name: String;
-
-    App::new("rename_device")
-        .author("Macku Jan <jamacku@redhat.com>")
-        .about("Program rename_device reads ENV INTERFACE, which is expected to contain the name of the network interface. Then it looks for the hardware address of such an interface. After that it looks at the kernel command line for key-value-pair ifname=NEW_NAME:MAC_ADDRESS with given mac address. If a new name wasn't found and kernel cmd it scans ifcfg configuration files in directory /etc/sysconfig/network-scripts/ and looks for configuration with HWADDR set to given hw address. If the program successfully finds such a configuration, it returns on standard output content of property DEVICE from matching ifcfg configuration. In all other cases it returns error code 1.")
-        .get_matches();
 
     /* Read env variable INTERFACE in order to get names of if */
     kernel_if_name = match read_env_interface(ENV) {
