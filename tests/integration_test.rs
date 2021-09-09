9c4e0a4d8db72d1de7bc9ed23050f22c49f3b99b
Jan Macku <jamacku@redhat.com>
2021-09-09 09:58:17 +0200

    integration testing
    add srade package
    serialize json

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,7 +3,34 @@
 use assert_cmd::Command; // Add methods on commands
 // use predicates::prelude::*; // Used for writing assertions
 use std::path::Path;
-use std::fs::{self};
+use std::fs::{
+    self
+};
+
+use serde::{
+    Deserialize,
+    Serialize
+};
+
+#[derive(Serialize, Deserialize)]
+struct Dataset {
+    name: String,
+    description: String,
+    input: DatasetInput,
+    output: DatasetOutput
+}
+
+#[derive(Serialize, Deserialize)]
+struct DatasetInput {
+    interface: String,
+    hw_address: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct DatasetOutput {
+    should_fail: bool,
+    expected_name: String
+}
 
 #[test]
 fn integration_test() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,18 +45,22 @@ fn integration_test() -> Result<(), Box<dyn std::error::Error>> {
 
         /* For each dataset; load configuration and run ifcfg_devname binary */
         if path.is_dir() {
-            let config_path = Path::new(format!("{}/config.json", path.unwrap()));
-            config_path.push("/config.json");
-            let cmdline_path = dataset_path.clone().push("/cmdline");
-            let ifcfgs_dir_path = dataset_path.clone().push("/ifcfgs"); 
+            let config_path = path.join("about.json");
+            let cmdline_path = path.join("cmdline");
+            let ifcfgs_dir_path = path.join("ifcfgs");
 
-            println!("path: {:?}", config_path);
-
-            //let foo = fs::read_to_string(config_path)?;
+            /* Read JSON configuration and then serialize it using srade_json */
+            let dataset_configuration: Dataset = serde_json::from_str(
+                &fs::read_to_string(config_path)?
+            )?;
 
             let assert = cmd
-                .env("INTERFACE", "new_name")
-                .args(&["./cmdlines/cmdline", "./ifcfgs/", "AA:BB:CC:DD:EE:FF"])
+                .env("INTERFACE", dataset_configuration.input.interface)
+                .args(&[
+                    cmdline_path.into_os_string().into_string().unwrap(), 
+                    ifcfgs_dir_path.into_os_string().into_string().unwrap(),
+                    dataset_configuration.input.hw_address
+                ])
                 .assert();
             assert.failure().code(1);
         }
