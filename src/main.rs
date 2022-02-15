12e9800b9ebfbbf10a315222229f60d0fcb895cc
Jan Macku <jamacku@redhat.com>
2022-02-15 10:39:58 +0100

    core: Drop code related to kernel-cmdline

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ mod parser;
 mod scanner;
 
 enum Args {
-    KernelCmdline = 1,
-    ConfigDir,
+    ConfigDir = 1,
     Mac,
     Length
 }
@@ -43,52 +42,35 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     let simple_mac_address = mac_address.to_string().to_lowercase();
 
-    let kernel_cmdline = lib::get_kernel_cmdline(is_test_mode, &args, Args::KernelCmdline as usize);
+    let config_dir = if !is_test_mode {
+        CONFIG_DIR
+    } else {
+        &args[Args::ConfigDir as usize]
+    };
 
-    /* Check kernel cmdline and also process ifname= entries */
-    let mut device_config_name = match parser::kernel_cmdline(&simple_mac_address, kernel_cmdline) {
-        Ok(Some(name)) => {
-            if lib::is_like_kernel_name(&name) {
-                warn!("Don't use kernel names (eth0, etc.) as new names for network devices! Used name: '{}'", name);
-            }
-            name
-        }
-        _ => {
-            debug!("New device name for '{}' wasn't found at kernel cmdline", kernel_interface_name);
-            String::from("")
+    let config_dir_path = Path::new(config_dir);
+    let ifcfg_paths = match scanner::config_dir(config_dir_path) {
+        Some(val) => val,
+        None => {
+            error!("Fail to get list of ifcfg files from directory {}", config_dir);
+            std::process::exit(1)
         }
     };
 
-    if device_config_name.is_empty() {
-        let config_dir = if !is_test_mode {
-            CONFIG_DIR
-        } else {
-            &args[Args::ConfigDir as usize]
-        };
+    let mut device_config_name = String::new();
+    
+    for path in ifcfg_paths {
+        let config_file_path: &Path = Path::new(&path);
 
-        let config_dir_path = Path::new(config_dir);
-        let ifcfg_paths = match scanner::config_dir(config_dir_path) {
-            Some(val) => val,
-            None => {
-                error!("Fail to get list of ifcfg files from directory {}", config_dir);
-                std::process::exit(1)
-            }
-        };
-
-        device_config_name = String::new();
-        for path in ifcfg_paths {
-            let config_file_path: &Path = Path::new(&path);
-
-            match parser::config_file(config_file_path, &simple_mac_address) {
-                Ok(Some(name)) => {
-                    if lib::is_like_kernel_name(&name) {
-                        warn!("Don't use kernel names (eth0, etc.) as new names for network devices! Used name: '{}'", name);
-                    }
-                    device_config_name = format!("{}", name);
-                    break;
+        match parser::config_file(config_file_path, &simple_mac_address) {
+            Ok(Some(name)) => {
+                if lib::is_like_kernel_name(&name) {
+                    warn!("Don't use kernel names (eth0, etc.) as new names for network devices! Used name: '{}'", name);
                 }
-                _ => continue,
+                device_config_name = format!("{}", name);
+                break;
             }
+            _ => continue,
         }
     }
 
